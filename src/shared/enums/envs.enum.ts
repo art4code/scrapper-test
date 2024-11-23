c82a23f66ae55691c95e52cc08ab6948241731fa
SR <aart4work@gmail.com>
2024-11-23 18:52:07 +0200

    chore: update env.enum

--- src/shared/enums/envs.enum.ts
+++ src/shared/enums/envs.enum.ts
@@ -1,14 +1,17 @@
 // !!! Update the .env.example file when adding new values to this enum and vice versa !!!
 export enum EnvEnum {
+  // App settings
   APP_LISTEN_PORT = "APP_LISTEN_PORT",
+  CORS_ORIGIN = "CORS_ORIGIN",
+  // JWT_SECRET = "JWT_SECRET",
 
+  // Proxy settings
+  SBR_WS_URL = "SBR_WS_URL",
+
+  // Database settings
   // PG_HOST = "PG_HOST",
   // PG_PORT = "PG_PORT",
   // PG_USER = "PG_USER",
   // PG_PASS = "PG_PASS",
   // PG_DB = "PG_DB",
-
-  CORS_ORIGIN = "CORS_ORIGIN",
-
-  // JWT_SECRET = "JWT_SECRET",
 }
